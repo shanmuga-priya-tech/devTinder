8391ff0294256512bb4e1ab8e4a707482e9a3279
shany-shan <priyaeswaran2023@gmail.com>
2024-09-19 21:58:23 +0530

    created a error handling middleware to catch any unexpected error

--- src/app.js
+++ src/app.js
@@ -11,6 +11,13 @@ app.get("/admin/getAllUser", (req, res) => {
   res.send("All user data");
 });
 
+//error handling middleware
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("something went wrong");
+  }
+});
+
 app.listen(7777, () => {
   console.log("server running on 7777...🥳");
 });
