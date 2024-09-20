83dc89762cd845f92b42fa492b623d005c10f99e
shany-shan <priyaeswaran2023@gmail.com>
2024-09-20 18:18:44 +0530

    created a first api to add user data to db

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const User = require("./models/userModel");
+
+//built-in middleware to convert json to js obj
+app.use(express.json());
+
+app.post("/user", async (req, res) => {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("user created successfully");
+  } catch (err) {
+    //console.log(err);
+    res.status(400).send("failed to create user");
+  }
+});
 
 connectDB()
   .then(() => {
