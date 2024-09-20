813fe2a292586380e28987645eaca55b159c38f9
shany-shan <priyaeswaran2023@gmail.com>
2024-09-20 11:03:15 +0530

    connected to db

--- src/app.js
+++ src/app.js
@@ -1,23 +1,14 @@
 const express = require("express");
-const { adminAuth } = require("./middlewares/auth");
-
+const connectDB = require("./config/db");
 const app = express();
 
-//this middleware fn runs for all the route that starts with "/admin"
-app.use("/admin", adminAuth);
-
-//if the middleware fn didn't send the res will continue to this fn
-app.get("/admin/getAllUser", (req, res) => {
-  res.send("All user data");
-});
-
-//error handling middleware
-app.use("/", (err, req, res, next) => {
-  if (err) {
-    res.status(500).send("something went wrong");
-  }
-});
-
-app.listen(7777, () => {
-  console.log("server running on 7777...🥳");
-});
+connectDB()
+  .then(() => {
+    console.log("DB connection successfull...🥳");
+    app.listen(7777, () => {
+      console.log("server running on 7777...🥳");
+    });
+  })
+  .catch((err) => {
+    console.log("error while connecting DB", err);
+  });
