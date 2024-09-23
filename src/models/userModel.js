c9d2b62437b3b4a3a6af04f2b7acf69e8d002dae
shany-shan <priyaeswaran2023@gmail.com>
2024-09-23 10:38:54 +0530

    added more schema validation for each field

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,17 +1,73 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  age: {
-    type: Number,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: [4, "FirstName should be atleast 4 characters long"],
+      maxLength: [10, "FirstName should not be more than 10 charcters"],
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxLength: [10, "lastName should not be more than 10 charcters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator: (value) => {
+          const emailRegex =
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+          // Test the email against the regex
+          if (emailRegex.test(email)) {
+            return true; // Email is valid
+          } else {
+            return false; // Email is invalid
+          }
+        },
+        message:
+          "Email ID is not in correct format.It should be something like xyz@gmail.com",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      required: [true, "Gender is required"],
+      enum: {
+        values: ["male", "female", "others"], // Valid enum values
+        message: `${value} is not supported. Please select male, female, or others.`,
+      },
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: [18, "You should be atleast 18 years old!"],
+      max: [75, "Age should be below 75"],
+    },
+    about: {
+      type: String,
+      default: "This is the default about you can edit it if you wish.",
+      maxLength: [200, "about can contain 200 characters"],
+    },
+    skills: {
+      type: [String],
+    },
   },
-  gender: {
-    type: String,
-  },
-  createdAt: { type: Date, default: Date.now },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
