36c19cbf974765208bcde16c33839a897e12908a
shany-shan <priyaeswaran2023@gmail.com>
2024-09-25 10:46:32 +0530

    added an another  api level  validation check

--- src/routes/connectinReqRouter.js
+++ src/routes/connectinReqRouter.js
@@ -5,16 +5,17 @@ const ConnectionReq = require("../models/connectionRequestModel");
 
 const requestRouter = express.Router();
 
+//api to send connection request
 requestRouter.post("/send/:status/:toUserId", userAuth, async (req, res) => {
   try {
-    const fromUserId = req.user;
+    const fromUserId = req.user._id;
     const toUserId = req.params.toUserId;
     const status = req.params.status;
 
     //1)check whether the touser is present or not
     const toUser = await User.findById({ _id: toUserId });
     if (!toUser) {
-      throw new Error("Invalid User");
+      throw new Error("User not found");
     }
 
     //2)check whether the status is either interested || ignored.
@@ -24,7 +25,12 @@ requestRouter.post("/send/:status/:toUserId", userAuth, async (req, res) => {
       throw new Error(`${status} not allowed`);
     }
 
-    //3)check whether the connection req is already established between to users
+    //3)check whether the user is trying to send connection request to himself
+    if (fromUserId.equals(toUserId)) {
+      throw new Error("You cannot connect with Yourself!");
+    }
+
+    //4)check whether the connection req is already established between to users
     const existingConnection = await ConnectionReq.findOne({
       $or: [
         { fromUserId, toUserId }, //user 1->user 2
@@ -32,7 +38,7 @@ requestRouter.post("/send/:status/:toUserId", userAuth, async (req, res) => {
       ],
     });
     if (existingConnection) {
-      throw new Error("Connection already established");
+      throw new Error("Connection request already established");
     }
 
     //creating new connection
@@ -44,7 +50,7 @@ requestRouter.post("/send/:status/:toUserId", userAuth, async (req, res) => {
 
     await data.save();
     res.json({
-      message: "connection established successfully!",
+      message: `${req.user.firstName} - ${status} - ${toUser.firstName} `,
       data,
     });
   } catch (err) {
