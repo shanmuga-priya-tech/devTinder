2aa61a8a18049f4015003bae9632480562547069
shany-shan <priyaeswaran2023@gmail.com>
2024-10-02 08:27:23 +0530

    logout bug fix

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -79,6 +79,9 @@ authRouter.post("/login", async (req, res) => {
 authRouter.post("/logout", (req, res) => {
   //expire the cookie
   res.cookie("token", null, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
     expires: new Date(Date.now()),
   });
 
