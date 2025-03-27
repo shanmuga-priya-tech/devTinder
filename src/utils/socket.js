d5f61a8c5bce90321f5e817bcdb524644a68c24c
shany-shan <priyaeswaran2023@gmail.com>
2025-03-27 18:54:52 +0530

    created a chat schema and added a logic to store the messages in db

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 const socket = require("socket.io");
 const crypto = require("crypto");
+const Chat = require("../models/chatModel");
 
 //fn to hash the roomid for security
 const getSecretRoomId = (userId, receiverId) => {
@@ -10,14 +11,14 @@ const getSecretRoomId = (userId, receiverId) => {
 };
 
 const createSocketConnection = (server) => {
-  //socket configuration
+  //1)socket configuration
   const io = socket(server, {
     cors: {
       origin: process.env.FRONTEND_URL,
     },
   });
 
-  //establishing connection
+  //2)establishing connection and listening up multiple events
   io.on("connection", (socket) => {
     //handle events
     socket.on("joinChat", ({ firstName, userId, receiverId }) => {
@@ -28,9 +29,37 @@ const createSocketConnection = (server) => {
 
     socket.on(
       "sendMessage",
-      ({ firstName, profilepic, userId, receiverId, msg }) => {
+      async ({ firstName, profilepic, userId, receiverId, msg }) => {
         const roomId = getSecretRoomId(userId, receiverId);
-        io.to(roomId).emit("messageReceived", { firstName, profilepic, msg });
+
+        //save msg in db
+        try {
+          //1) trying to find if there is any existing enteries for these two particpants
+          let chat = await Chat.findOne({
+            participants: { $all: [userId, receiverId] },
+          });
+
+          //2)if not trying to create new entry
+          if (!chat) {
+            chat = new Chat({
+              participants: [userId, receiverId],
+              messages: [],
+            });
+          }
+          //3)no matter whether its a new entry or existing entry we need to append the new msg to msgs array
+          chat.messages.push({
+            senderId: userId,
+            text,
+          });
+
+          //saving msg in db
+          await chat.save();
+
+          //sending the msg to a room
+          io.to(roomId).emit("messageReceived", { firstName, profilepic, msg });
+        } catch (err) {
+          console.log(err);
+        }
       }
     );
 
